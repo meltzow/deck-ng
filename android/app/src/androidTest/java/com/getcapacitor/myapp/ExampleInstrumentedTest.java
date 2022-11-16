9f59e9cb1d818e72b1c05feae1041187fadde599
mario meltzow <dev@meltzow.net>
2022-11-16 12:40:13 +0100

    current version of getting snapshots

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -1,11 +1,24 @@
 package com.getcapacitor.myapp;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.clearElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webKeys;
 
 import android.content.Context;
+
+import androidx.test.espresso.web.assertion.WebViewAssertions;
+import androidx.test.espresso.web.model.Atom;
+import androidx.test.espresso.web.model.ElementReference;
+import androidx.test.espresso.web.webdriver.DriverAtoms;
+import androidx.test.espresso.web.webdriver.Locator;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -40,31 +53,53 @@ public class ExampleInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
-//    @BeforeClass
-//    public static void beforeAll() {
-//      CleanStatusBar.enableWithDefaults();
-//    }
-//
-//    @AfterClass
-//    public static void afterAll() {
-//      CleanStatusBar.disable();
-//    }
-
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("net.meltzow.deckng", appContext.getPackageName());
+    @BeforeClass
+    public static void beforeAll() {
+      CleanStatusBar.enableWithDefaults();
     }
 
+    @AfterClass
+    public static void afterAll() {
+      CleanStatusBar.disable();
+    }
+
+//    @Test
+//    public void useAppContext() throws Exception {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//
+//        assertEquals("net.meltzow.deckng", appContext.getPackageName());
+//    }
+
   @Test
-  public void testTakeScreenshot() {
+  public void testTakeScreenshot() throws InterruptedException {
     Screengrab.setDefaultScreenshotStrategy(new FalconScreenshotStrategy(activityRule.getActivity()));
-    // Your custom onView...
-//    onView(withId(R.id.fab)).perform(click());
+
+    // find main menu button and click it
+    String urlInputXPath = "//input[@name='url']";
+    String usernameInput= "//input[@name='username']";
+    String passwordInput= "//input[@name='password']";
+    String loginBtn= "//ion-button[@type='submit']";
+
+    Atom<ElementReference> element4 = findElement(Locator.XPATH, passwordInput);
+    onWebView().withElement(element4).perform(webKeys("admin"));
+
+    Atom<ElementReference> element2 = findElement(Locator.XPATH, usernameInput);
+    onWebView().withElement(element2).perform(webKeys("admin"));
+
+    Atom<ElementReference> element1 = findElement(Locator.XPATH, urlInputXPath);
+    onWebView().withElement(element1).perform(webKeys("http://192.168.178.25:8080"));
 
     Screengrab.screenshot("LoginScreen");
+
+    Atom<ElementReference> element3 = findElement(Locator.XPATH, loginBtn);
+    onWebView().withElement(element3).perform(webClick());
+
+    Thread.sleep(2000);
+
+    Screengrab.screenshot("BoardsScreen");
+
   }
+
 
 }
