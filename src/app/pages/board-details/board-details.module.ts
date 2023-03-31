bf752ccc6604e70852e87acfdf1e1a1d3904053f
mario meltzow <dev@meltzow.net>
2023-03-31 09:10:30 +0200

    add translation

--- src/app/pages/board-details/board-details.module.ts
+++ src/app/pages/board-details/board-details.module.ts
@@ -6,15 +6,17 @@ import { IonicModule } from '@ionic/angular';
 import { CardPreviewComponent } from './card-preview/card-preview.component';
 import { BoardDetailsPageRoutingModule } from "@app/pages/board-details/board-details-routing.module";
 import { SharedModule } from "@app/shared.module";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    BoardDetailsPageRoutingModule,
-    SharedModule,
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        BoardDetailsPageRoutingModule,
+        SharedModule,
+        TranslateModule,
+    ],
     declarations: [BoardDetailsPage, CardPreviewComponent]
 })
 export class BoardDetailsModule {}
