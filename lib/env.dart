a9efdfe8e65577d14be875b3c7f69804824679d5
mario <dev@meltzow.net>
2024-08-16 08:52:11 +0200

    first step

---
+++ lib/env.dart
@@ -0,0 +1,9 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@envied
+abstract class Env {
+  // @EnviedField(varName: 'WIREDASH_SECRET')
+//  static const wiredashSecrret = _Env.wiredashSecret;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:deck_ng/my_app.dart';
+import 'package:envied/envied.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
@@ -8,5 +9,6 @@ Future<void> main() async {
   await GetStorage.init();
 
   runApp(MyApp(debugShowCheckedModeBanner: kDebugMode ? true : false));
-
 }
+
+
