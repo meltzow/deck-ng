c71e3ca302f8fd207fac1bf7ccba697e60c880c9
mario meltzow <dev@meltzow.net>
2024-09-14 20:26:05 +0200

    working now

--- lib/screen/card_details_screen.dart
+++ lib/screen/card_details_screen.dart
@@ -7,11 +7,18 @@ import 'package:get/get.dart';
 class CardDetailsScreen extends StatelessWidget {
   final CardDetailsController cardController =
       Get.find<CardDetailsController>();
+  final FocusNode descriptionFocusNode = FocusNode();
 
   CardDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    descriptionFocusNode.addListener(() {
+      if (!descriptionFocusNode.hasFocus) {
+        cardController.isEditMode.value = false;
+      }
+    });
+
     return Obx(() {
       final card = cardController.card.value;
       if (card == null) {
@@ -29,93 +36,117 @@ class CardDetailsScreen extends StatelessWidget {
           ),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: ListView(
-              children: [
-                TextField(
-                  controller: cardController.titleController,
-                  decoration: const InputDecoration(labelText: 'Title'),
-                ),
-                const SizedBox(height: 16.0),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Obx(() {
-                        if (cardController.isEditMode.value) {
-                          return TextField(
-                            controller: cardController.descriptionController,
-                            maxLines: null,
-                            onChanged: (text) {
-                              cardController.updateMarkdownPreview(text);
-                            },
-                            onEditingComplete: () {
-                              cardController.isEditMode.value = false;
-                            },
-                          );
-                        } else {
-                          return GestureDetector(
-                            onTap: () {
-                              cardController.isEditMode.value = true;
-                            },
-                            child: SingleChildScrollView(
-                              child: MarkdownBody(
-                                data: cardController.markdownPreview.value,
-                              ),
-                            ),
-                          );
-                        }
-                      }),
-                    ),
-                    IconButton(
-                      icon: Icon(cardController.isEditMode.value
-                          ? Icons.preview
-                          : Icons.edit),
-                      onPressed: () {
-                        cardController.isEditMode.value =
-                            !cardController.isEditMode.value;
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16.0),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: cardController.duedateController,
-                        decoration:
-                            const InputDecoration(labelText: 'Due Date'),
-                        readOnly: true,
-                        onTap: () async {
-                          DateTime? selectedDate = await showDatePicker(
-                            context: context,
-                            initialDate: card.duedate ?? DateTime.now(),
-                            firstDate: DateTime(2000),
-                            lastDate: DateTime(2101),
-                          );
-                          if (selectedDate != null) {
-                            cardController.duedateController.text =
-                                selectedDate.toIso8601String();
-                            cardController.card.value =
-                                card.copyWith(duedate: selectedDate);
-                          }
-                        },
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: cardController.titleController,
+                    decoration: const InputDecoration(labelText: 'Title'),
+                  ),
+                  const SizedBox(height: 16.0),
+                  _buildDescriptionField(context, card),
+                  const SizedBox(height: 16.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: cardController.duedateController,
+                          decoration:
+                              const InputDecoration(labelText: 'Due Date'),
+                          readOnly: true,
+                          onTap: () async {
+                            DateTime? selectedDate = await showDatePicker(
+                              context: context,
+                              initialDate: card.duedate ?? DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2101),
+                            );
+                            if (selectedDate != null) {
+                              cardController.duedateController.text =
+                                  selectedDate.toIso8601String();
+                              cardController.card.value =
+                                  card.copyWith(duedate: selectedDate);
+                            }
+                          },
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.clear),
-                      onPressed: cardController.clearDueDate,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16.0),
-                _buildLabelsField(context, card),
-                const SizedBox(height: 16.0),
-                _buildAssignedUsersField(context, card),
-              ],
+                      IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: cardController.clearDueDate,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16.0),
+                  _buildLabelsField(context, card),
+                  const SizedBox(height: 16.0),
+                  _buildAssignedUsersField(context, card),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      cardController.updateCard(card);
+                    },
+                    child: const Text('Save'),
+                  ),
+                ],
+              ),
             ),
           ),
         );
       }
+    });
+  }
+
+  Widget _buildDescriptionField(BuildContext context, card_model.Card card) {
+    return Obx(() {
+      return Row(
+        children: [
+          Expanded(
+            child: cardController.isEditMode.value
+                ? SingleChildScrollView(
+                    child: TextField(
+                      controller: cardController.descriptionController,
+                      focusNode: descriptionFocusNode,
+                      decoration:
+                          const InputDecoration(labelText: 'Description'),
+                      maxLines: null,
+                      onChanged: (text) {
+                        cardController.updateMarkdownPreview(text);
+                      },
+                    ),
+                  )
+                : GestureDetector(
+                    onTap: () {
+                      cardController.isEditMode.value = true;
+                      descriptionFocusNode.requestFocus();
+                    },
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Description'),
+                          MarkdownBody(
+                            data: cardController.markdownPreview.value,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+          ),
+          IconButton(
+            icon: Icon(
+                cardController.isEditMode.value ? Icons.preview : Icons.edit),
+            onPressed: () {
+              cardController.isEditMode.value =
+                  !cardController.isEditMode.value;
+              if (cardController.isEditMode.value) {
+                descriptionFocusNode.requestFocus();
+              } else {
+                descriptionFocusNode.unfocus();
+              }
+            },
+          ),
+        ],
+      );
     });
   }
 
